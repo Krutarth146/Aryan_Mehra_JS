a48d7ca45dc02bc4ce5970953a267a1d77d56252
Krutarth Patel <krutarthpatel146@gmail.com>
2023-08-09 22:15:31 +0530

    Promises

--- promises2.js
+++ promises2.js
@@ -1,30 +1,42 @@
 console.log("Process Started");
 
-function getPassport(callback) {
-  setTimeout(() => {
-    const passport = "😶‍🌫️";
-    console.log("Here is a Passport", passport);
-    callback(passport);
-  }, 2000);
+function getPassport() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const passport = '👌';
+      resolve(passport);
+    }, 2000);
+  })
 }
 
-function getVisa(passport, callback) {
-  setTimeout(() => {
-    const visa = passport + "👍";
-    console.log("Here is a Visa", visa);
-    callback(visa);
-  }, 2000);
+function getVisa(passport) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      const visa = passport + '🤣';
+      // resolve(visa)
+      reject(visa)
+    },2000);
+  })
 }
 
 // console.clear();
 async function ApVisa() {
-  const passport = await getPassport();
-  console.log("Here is a Passport", passport);
 
-  const visa = await getVisa();
-  console.log("Here is a Visa", visa);
+  try{
+    const passport = await getPassport();
+    console.log("Here is a Passport", passport);
 
+    const visa = await getVisa();
+    console.log("Here is a Visa", visa);
+  }
+  
+  catch(err)
+  {
+    console.log("Error Occured",err);
+  }
   console.log("Process Ended");
+  
 }
 
 ApVisa();
+
